d2ca606fdfbec09a31ca04333ba51dabd163b82e
olyaishere <o.sheredeko@gmail.com>
2023-03-05 17:50:37 +0000

    Updated time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+function formatDate(timestamp) {
+  let date = new Date();
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function showTemperature(response) {
   console.log(response.data);
 
@@ -15,6 +40,9 @@ function showTemperature(response) {
 
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.time);
 }
 
 let apiKey = "88aae3fb0f0t59d8474d30o302905c2b";
