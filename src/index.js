354cc4bcc56922257e6169d39ea9ebf11c7c923d
olyaishere <o.sheredeko@gmail.com>
2023-03-05 17:21:02 +0000

    Showing the current temperature in selected city

---
+++ src/index.js
@@ -0,0 +1,23 @@
+function showTemperature(response) {
+  console.log(response.data);
+
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "88aae3fb0f0t59d8474d30o302905c2b";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(showTemperature);
